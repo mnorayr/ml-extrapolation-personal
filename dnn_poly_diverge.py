d7e89539c9f51171d39755ad81c2534deafd32fd
kimmmx <max.kimmet@comed.com>
2017-12-18 09:54:36 -0600

    Added text file with model parameters, uses epochs_per_iteration

--- dnn_poly_diverge.py
+++ dnn_poly_diverge.py
@@ -39,6 +39,12 @@ def create_model(params):
 
     """
     # Create and train model
+    epochs_per_iteration = 0
+    try:
+        epochs_per_iteration = params.pop('epochs_per_iteration')
+        params['train_samples_per_iteration'] = int(epochs_per_iteration * len(df_train_val) / 1.258)
+    except KeyError:
+        pass
     model = H2ODeepLearningEstimator(**params)
     model.train(x='x', y='y', training_frame=train, validation_frame=val)
 
@@ -47,23 +53,52 @@ def create_model(params):
     pred_test = model.predict(test).as_data_frame()
 
     # Plot real data
-    plt.plot(df_train_val['x'], df_train_val['y'])
-    plt.plot(df_test['x'][:len(df_test)/2], df_test['y'][:len(df_test)/2])
-    plt.plot(df_test['x'][len(df_test)/2:], df_test['y'][len(df_test)/2:])
+    plt.figure()
+    plt.plot(df_train_val['x'], df_train_val['y'], color='orange')
+    plt.plot(df_test['x'][:len(df_test)/2], df_test['y'][:len(df_test)/2], color='orange')
+    plt.plot(df_test['x'][len(df_test)/2:], df_test['y'][len(df_test)/2:], color='orange')
 
     # Plot model predictions
-    plt.plot(df_train_val['x'], pred_train_val)
-    plt.plot(df_test['x'][:len(df_test)/2], pred_test[:len(pred_test)/2])
-    plt.plot(df_test['x'][len(df_test)/2:], pred_test[len(pred_test)/2:])
+    plt.plot(df_train_val['x'], pred_train_val, color='blue')
+    plt.plot(df_test['x'][:len(df_test)/2], pred_test[:len(pred_test)/2], color='blue')
+    plt.plot(df_test['x'][len(df_test)/2:], pred_test[len(pred_test)/2:], color='blue')
 
     # Get model metrics
     test_rmse = '{:.1f}'.format(model.model_performance(test).rmse())
     train_val_rmse = '{:.1f}'.format(model.model_performance(train_val).rmse())
-    samples_per_iteration = int(round(model.train_samples_per_iteration*1.258/len(df_train_val)))
 
-    # Save model and plot
-    name = '{0}_{1}_{2}_{3}'.format(test_rmse, train_val_rmse, model.activation, samples_per_iteration)
+    # Define name for saving model, model parameters, plot
+    name = '{0}_{1}_{2}_{3}_{4}'.format(test_rmse, train_val_rmse, model.activation, model.hidden, epochs_per_iteration)
+
+    # Save model and model parameters
     h2o.save_model(model, os.path.join(save_dir, name))
+    with open('{}.txt'.format(os.path.join(save_dir, name)), 'wb') as f:
+        f.write('test_rmse = {}\n'.format(test_rmse))
+        f.write('train_val_rmse = {}\n\n'.format(train_val_rmse))
+        f.write('epochs = {}\n'.format(model.epochs))
+        f.write('hidden = {}\n'.format(model.hidden))
+        f.write('activation = {}\n'.format(model.activation))
+        f.write('hidden_dropout_ratios = {}\n'.format(model.hidden_dropout_ratios))
+        f.write('l1 = {}\n'.format(model.l1))
+        f.write('l2 = {}\n'.format(model.l2))
+        f.write('max_w2 = {}\n'.format(model.max_w2))
+        f.write('stopping_rounds = {}\n'.format(model.stopping_rounds))
+        f.write('stopping_tolerance = {}\n'.format(model.stopping_tolerance))
+        f.write('stopping_metric = {}\n'.format(model.stopping_metric))
+        f.write('score_interval = {}\n'.format(model.score_interval))
+        f.write('score_duty_cycle = {}\n'.format(model.score_duty_cycle))
+        f.write('epochs_per_iteration = {}\n'.format(epochs_per_iteration))
+        f.write('adaptive_rate = {}\n'.format(model.adaptive_rate))
+        f.write('rho = {}\n'.format(model.rho))
+        f.write('epsilon = {}'.format(model.epsilon))
+        f.write('rate = {}\n'.format(model.rate))
+        f.write('rate_annealing = {}\n'.format(model.rate_annealing))
+        f.write('momentum_start = {}\n'.format(model.momentum_start))
+        f.write('momentum_ramp = {}\n'.format(model.momentum_ramp))
+        f.write('momentum_stable = {}\n'.format(model.momentum_stable))
+        f.write('initial_weight_distribution = {}\n'.format(model.initial_weight_distribution))
+
+    # Save plot
     plt.savefig('{}.svg'.format(os.path.join(save_dir, name)))
 
     # Close plot
@@ -77,14 +112,14 @@ def main():
         dict(
             model_id='dnn_poly',
             epochs=5000,
-            hidden=[300, 300],
+            hidden=[1000],
             activation='maxout_with_dropout',
-            hidden_dropout_ratios=[0.5, 0.5],
+            hidden_dropout_ratios=[0.1],
             l1=1e-4,
             l2=1e-4,
-            max_w2=0.55,
+            max_w2=0.2,
             stopping_rounds=10,
-            # stopping_tolerance=1e-4,
+            stopping_tolerance=1e-4,
             stopping_metric='rmse',
 
             # Control scoring epochs
@@ -92,9 +127,11 @@ def main():
             score_duty_cycle=1,
             shuffle_training_data=False,
             replicate_training_data=True,
-            train_samples_per_iteration=int(1 * len(df_train_val) / 1.258),
+            epochs_per_iteration=1,
 
             # Controlling momentum
+            rate=1e-5,
+            rate_annealing=1e-10,
         ),
     ]
 
@@ -114,8 +151,8 @@ if __name__ == '__main__':
         os.makedirs(save_dir)
 
     # Define characteristics of training and test data
-    train_domain = 5
-    test_domain = 10
+    train_domain = 10
+    test_domain = 20
     density = 10000
 
     # Generate training and test sets
